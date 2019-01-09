e7bf83987e39649a00e7d1019f0fb375f0a0bead
Danny Peng <dannyw.peng@gmail.com>
2019-01-09 04:12:41 -0800

    added js to return to top of page after refreshing

--- js/application.js
+++ js/application.js
@@ -1,3 +1,7 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
 // typing and deleting skills effect
 window.onload = function() {
   var el = document.getElementById('typewrite');
